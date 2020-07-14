2ff2dd1dc27bd77aa5c74ac7a40972b31befa81f
Antonio Jackson <antoniojackson@gmail.com>
2020-07-14 09:44:19 -0600

    Setting up connection with the API and parsing data into @members

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
     nation = params[:nation].gsub('_','+')
+    conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com')
+    response = conn.get("/api/v1/characters?affiliation=#{nation}")
+    @members = JSON.parse(response.body, symbolize_names: true)
   end
 end
