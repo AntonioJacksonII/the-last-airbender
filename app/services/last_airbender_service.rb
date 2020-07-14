d1f10061e0b73bcd4752ce3401d7cc8b81c4c2dd
Antonio Jackson <antoniojackson@gmail.com>
2020-07-14 10:30:12 -0600

    Refactoring connection with the API into a separate method

--- app/services/last_airbender_service.rb
+++ app/services/last_airbender_service.rb
@@ -1,7 +1,10 @@
 class LastAirbenderService
 
+  def conn
+    Faraday.new(url: 'https://last-airbender-api.herokuapp.com')
+  end
+
   def members_by_nation(nation)
-    conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com')
     response = conn.get("/api/v1/characters?affiliation=#{nation}")
     JSON.parse(response.body, symbolize_names: true)
   end
